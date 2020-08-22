007f14828c049b1e41ab0ab7fd5eeee3a359bf7f
tsubasa1122 <nakanot@open8.com>
2020-08-22 19:18:27 +0900

    Create POST Todos

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,9 @@
 import express, { Request, Response, NextFunction } from 'express';
 import todoRoutes from './routes/todos';
+import { json } from 'body-parser';
 
 const app = express();
+app.use(json());
 app.use('/todos', todoRoutes);
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
   res.status(500).json({ message: err.message });
---
+++ src/controllers/todos.ts
@@ -0,0 +1,12 @@
+import { RequestHandler } from 'express';
+import { Todo } from '../models/todo';
+
+const TODOS = [];
+
+export const createTodo: RequestHandler = (req, res, next) => {
+  const text = (req.body as { text: string }).text;
+  const newTodo = new Todo(Math.random().toString(), text);
+
+  TODOS.push(newTodo);
+  res.status(201).json({ message: 'TODOを作成しました。', createdTodo: newTodo });
+};
